7c64718cb02d3193b98dfa23e97e0fedbd537d30
lonewolfgames <nathanfaucett@gmail.com>
2014-07-13 12:24:43 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,49 +1,67 @@
-var http = require("http"),
-	STATUS_CODES = http.STATUS_CODES,
-	STATUS_NAMES = {},
-	
-	inflect = require("node-inflect");
+var STATUS_CODES = require("./status_codes");
 
-Object.keys(STATUS_CODES).forEach(function(code){
-	if (code < 400) return;
-	var name = inflect.camelize(STATUS_CODES[code]);
-	
-	if (!/\w+Error$/.test(name)) name += "Error";
-	STATUS_NAMES[code] = name;
+
+var STATUS_NAMES = {},
+    STATUS_STRINGS = {};
+
+
+Object.keys(STATUS_CODES).forEach(function(code) {
+    if (code < 400) return;
+    var status = STATUS_CODES[code],
+        name = status.replace(/\s+/g, "");
+
+    if (!(/\w+Error$/.test(name))) name += "Error";
+    STATUS_NAMES[code] = name;
+    STATUS_STRINGS[code] = status;
 });
 
 
 function HttpError(code, message) {
-	if (code instanceof Error) {
-		message = code.message;
-		code = 500;
-	} else {
-		code || (code = 500);
-	}
-	
-	Error.call(this);
-	
-	this.name = STATUS_NAMES[code];
-	this.statusCode = code;
-	this.message = this.name +": "+ code +" "+ (message || STATUS_CODES[code]);
+    if (!(this instanceof HttpError)) return new HttpError(code, message);
+
+    if (code instanceof Error) {
+        message = code.message;
+        code = 500;
+    } else if (typeof(code) === "string") {
+        message = code;
+        code = 500;
+    } else {
+        code || (code = 500);
+    }
+
+    Error.call(this);
+
+    this.name = STATUS_NAMES[code] || "UnknownHttpError";
+    this.statusCode = code;
+    this.message = this.name + ": " + code + " " + (message || STATUS_STRINGS[code]);
+
+    return this;
 }
 HttpError.prototype = Object.create(Error.prototype);
 HttpError.prototype.constructor = HttpError;
 
+HttpError.prototype.toString = function() {
 
-HttpError.prototype.toString = function(){
-	
-	return this.message;
+    return this.message;
 };
 
+HttpError.prototype.toJSON = function(json) {
+    json || (json = {});
 
-HttpError.prototype.toJSON = function(){
-	
-	return {
-		name: this.name,
-		statusCode: this.statusCode,
-		message: this.message
-	};
+    json.name = this.name;
+    json.statusCode = this.statusCode;
+    json.message = this.message;
+
+    return json;
+};
+
+HttpError.prototype.fromJSON = function(json) {
+
+    this.name = json.name;
+    this.statusCode = json.statusCode;
+    this.message = json.message;
+
+    return this;
 };
 
 
